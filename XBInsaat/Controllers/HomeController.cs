c84c3d341dff0effbb8fab6be645cde9f3890ba0
Elnur Ibrahimli <elnur204@gmail.com>
2023-07-05 16:23:44 +0400

    localization test

--- XBInsaat/Controllers/HomeController.cs
+++ XBInsaat/Controllers/HomeController.cs
@@ -1,16 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Resources;
+using XBInsaat.Services.Services.Implementations.User;
 
 namespace XBInsaat.Controllers
 {
     public class HomeController : Controller
     {
-        
-        public IActionResult Index()
+        private LanguageService _localization;
+        public HomeController(LanguageService localization)
         {
+            _localization = localization;
+        }
+        public IActionResult Change(string text, string language)
+        {
+         
+
+
             return View();
         }
-
+        public IActionResult Index()
+        {
+            ViewBag.Welcome = _localization.Getkey("GeneralText").Value;
+            var currentCulture = Thread.CurrentThread.CurrentCulture.Name;
+            return View();
+        }
        
+        public IActionResult ChangeLanguage(string culture)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)), new CookieOptions()
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1)
+                });
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+
+
     }
 }
--- XBInsaat/Program.cs
+++ XBInsaat/Program.cs
@@ -3,8 +3,40 @@ using System.Configuration;
 using XBInsaat.Mvc.ServiceExtentions;
 using Microsoft.EntityFrameworkCore;
 using FluentValidation.AspNetCore;
+using XBInsaat.Services.Services.Implementations.User;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using System.Reflection;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+#region Localizer
+builder.Services.AddSingleton<LanguageService>();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddMvc().AddViewLocalization().AddDataAnnotationsLocalization(options =>
+    options.DataAnnotationLocalizerProvider = (type, factory) =>
+    {
+        var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName);
+        return factory.Create(nameof(SharedResource), assemblyName.Name);
+    });
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportCultures = new List<CultureInfo>
+    {
+        new CultureInfo("az-Latn-AZ"),
+        new CultureInfo("en-US"),
+        new CultureInfo("ru-RU"),
+
+    };
+    options.DefaultRequestCulture = new RequestCulture(culture: "az-Latn-AZ", uiCulture: "az-Latn-AZ");
+    options.SupportedCultures = supportCultures;
+    options.SupportedUICultures = supportCultures;
+    options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+});
+#endregion
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -33,6 +65,7 @@ app.UseStaticFiles();
 
 app.AddExceptionHandlerService();
 
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 app.UseRouting();
 
 app.UseAuthentication();
